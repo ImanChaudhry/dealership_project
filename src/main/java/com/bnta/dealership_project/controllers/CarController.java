2cf824b6a14921bb84d8e081e7f1f51384e3b107
Iman Chaudhry <i-man-735@hotmail.com>
2022-05-19 10:58:49 +0100

    completed Car controller

--- src/main/java/com/bnta/dealership_project/controllers/CarController.java
+++ src/main/java/com/bnta/dealership_project/controllers/CarController.java
@@ -1,4 +1,47 @@
 package com.bnta.dealership_project.controllers;
 
+
+import com.bnta.dealership_project.models.Car;
+import com.bnta.dealership_project.repositories.CarRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("cars")
 public class CarController {
+
+    @Autowired
+    CarRepository carRepository;
+
+// INDEX
+
+    @GetMapping
+    public ResponseEntity<Car> getAllPetsAndFilters(
+            @RequestParam(required = false, name = "type") String type
+    ) {
+        if (type != null) {
+            return new ResponseEntity(carRepository.findAll(), HttpStatus.OK);
+        }
+    }
+
+
+// SHOW
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Optional<Car>> getCar (@PathVariable Long id){
+        return new ResponseEntity(carRepository.findById(id)), HttpStatus.OK);
+    }
+
+// POST
+
+    @PostMapping // localhost:8080/pets/
+    public ResponseEntity<Car> createCar(@RequestBody Car newCar){
+        carRepository.save(newCar);
+        return new ResponseEntity<>(newCar, HttpStatus.CREATED);
+    }
+
 }
