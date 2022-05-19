22659bba0348bbd1b5d4026cceeca4b45e2aa04a
Iman Chaudhry <i-man-735@hotmail.com>
2022-05-19 11:03:38 +0100

    completed both repositories

--- src/main/java/com/bnta/dealership_project/repositories/CarRepository.java
+++ src/main/java/com/bnta/dealership_project/repositories/CarRepository.java
@@ -1,4 +1,12 @@
 package com.bnta.dealership_project.repositories;
 
-public interface CarRepository {
+import com.bnta.dealership_project.models.Car;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CarRepository extends JpaRepository<Car, Long> {
+
+
+    List<Car> findCarByType(String type);
 }
--- src/main/java/com/bnta/dealership_project/repositories/CustomerRepository.java
+++ src/main/java/com/bnta/dealership_project/repositories/CustomerRepository.java
@@ -1,4 +1,7 @@
 package com.bnta.dealership_project.repositories;
 
-public interface CustomerRepository {
+import com.bnta.dealership_project.models.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
 }
